a6b64b1a9433c38b0c6ef70d6bd6cd7adef6615e
SonHongJoo <shj0917@naver.com>
2020-05-07 13:58:28 +0900

    Updates

---
+++ precache-manifest.d8c80a649fd4e9c1d8a93f50264b353b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7ec0a1364ce29c978337ac4bdce7cf",
+    "url": "/devlog/index.html"
+  },
+  {
+    "revision": "179f232fd54d6545c6c7",
+    "url": "/devlog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2200a3ea32f72065782",
+    "url": "/devlog/static/js/2.f722d598.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/devlog/static/js/2.f722d598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179f232fd54d6545c6c7",
+    "url": "/devlog/static/js/main.8c701c04.chunk.js"
+  },
+  {
+    "revision": "7193d8674181b9e94681",
+    "url": "/devlog/static/js/runtime-main.598e244f.js"
+  }
+]);
